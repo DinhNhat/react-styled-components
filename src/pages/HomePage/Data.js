fc7faf03f881655bd9232d249697bcf3c354ae59
Nhat Dinh <nguyendinhnhat206@gmail.com>
2021-04-18 21:34:24 -0400

    first commit

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,72 @@
+import lostOnline from '../../images/undraw_lost_online_wqob.svg';
+import Svg2 from '../../images/svg-2.svg';
+import Svg3 from '../../images/svg-3.svg';
+import Profile from '../../images/profile.jpg';
+
+export const homeObjOne = {
+    lightBg: false,
+    primary: true, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    descripton: `We help business owners increase their revenue. 
+                Our team of unique specialist can help you acheive your business goals.`, 
+    headline: `Lead Generation Speacialist for Online Bunisesses`, 
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: lostOnline,
+    alt: 'image',
+    start: ''
+};
+
+export const homeObjTwo = {
+    lightBg: true,
+    primary: false, 
+    imgStart: 'start', 
+    lightTopLine: false, 
+    lightTextDesc: false, 
+    buttonLabel: 'Get Started', 
+    descripton: `We help business owners increase their revenue. 
+                Our team of unique specialist can help you acheive your business goals.`, 
+    headline: `Lead Generation Speacialist for Online Bunisesses`, 
+    lightText: false,
+    topLine: 'Marketing Agency',
+    img: Profile,
+    alt: 'Image',
+    start: 'true'
+};
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    descripton: `We help business owners increase their revenue. 
+                Our team of unique specialist can help you acheive your business goals.`, 
+    headline: `Lead Generation Speacialist for Online Bunisesses`, 
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: Svg2,
+    alt: 'image',
+    start: ''
+};
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true, 
+    imgStart: 'start', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    descripton: `We help business owners increase their revenue. 
+                Our team of unique specialist can help you acheive your business goals.`, 
+    headline: `Lead Generation Speacialist for Online Bunisesses`, 
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: Svg3,
+    alt: 'image',
+    start: 'true'
+};
